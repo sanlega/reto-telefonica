13dd3fc14410ed68edbbda79023b03081456c3dc
Sanlega <slegaris@student.42madrid.com>
2023-07-06 20:47:12 +0200

    React

---
+++ challenge-chat/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { LoginPage, ChatPage } from "./pages";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/chat/:username" element={<ChatPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
